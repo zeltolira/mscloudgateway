c3780c77e176952bb8f7c43bd69f86c4dc21aaad
unknown <zelto@hotmail.com>
2024-03-06 01:32:34 -0300

    projeto finalizado

--- src/main/java/com/lira/mscloudgateway/MscloudgatewayApplication.java
+++ src/main/java/com/lira/mscloudgateway/MscloudgatewayApplication.java
@@ -23,6 +23,8 @@ public class MscloudgatewayApplication {
 		return builder
 				.routes()
 					.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+					.route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
+					.route(r -> r.path("/avaliacoes-credito/**").uri("lb://msavaliadorcredito"))
 				.build();
 	}
 }
